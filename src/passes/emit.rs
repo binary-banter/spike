88cb6aeda97d36a9507dd782a7a8ab3590f73d7c
jonathan <jonathantbrouwer@gmail.com>
2023-10-07 23:08:05 +0200

    Push & pop

--- src/passes/emit.rs
+++ src/passes/emit.rs
@@ -61,8 +61,22 @@ fn emit_instr(instr: &Instr<Arg>, machine_code: &mut Vec<u8>) {
             },
             dst,
         ),
-        Instr::Pushq { src: _ } => todo!(),
-        Instr::Popq { dst: _ } => todo!(),
+        Instr::Pushq { src } => {
+            encode_push_pop(PushPopInfo {
+                op_reg: 0x50,
+                op_deref: 0xFF,
+                op_imm: 0x68,
+                imm_as_src: 0x6,
+            }, src)
+        },
+        Instr::Popq { dst } => {
+            encode_push_pop(PushPopInfo {
+                op_reg: 0x58,
+                op_deref: 0x8F,
+                op_imm: 0, //Unreachable
+                imm_as_src: 0x0,
+            }, dst)
+        },
         Instr::Callq { lbl, arity } => match (*lbl, arity) {
             ("_print_int", 1) => todo!(),
             ("_read_int", 0) => todo!(),
@@ -74,6 +88,59 @@ fn emit_instr(instr: &Instr<Arg>, machine_code: &mut Vec<u8>) {
         Instr::Jmp { lbl: _ } => todo!(),
     };
     machine_code.extend(v);
+}
+
+struct PushPopInfo {
+    op_reg: u8,
+    op_deref: u8,
+    op_imm: u8,
+    imm_as_src: u8,
+}
+
+fn encode_push_pop(op_info: PushPopInfo, reg: &Arg) -> Vec<u8> {
+    match reg {
+        Arg::Imm { val } => {
+            let val = *val as i32;
+
+            let mut v = vec![
+                op_info.op_imm
+            ];
+            v.extend(val.to_le_bytes());
+            v
+        },
+        Arg::Reg { reg } => {
+            let (r, rrr) = encode_reg(reg);
+            if r == 0 {
+                vec![
+                    op_info.op_reg | rrr
+                ]
+            } else {
+                vec![
+                    0x41,
+                    op_info.op_reg | rrr
+                ]
+            }
+        }
+        Arg::Deref { reg, off } => {
+            let (r, rrr) = encode_reg(reg);
+            let off = *off as i32;
+
+            let mut v = if r == 0 {
+                vec![
+                    op_info.op_deref,
+                    0b10_000_000 | op_info.imm_as_src << 3 | rrr,
+                ]
+            } else {
+                vec![
+                    0x41,
+                    op_info.op_deref,
+                    0b10_000_000 | op_info.imm_as_src << 3 | rrr,
+                ]
+            };
+            v.extend(off.to_le_bytes());
+            v
+        }
+    }
 }
 
 struct UnaryOpInfo {
@@ -383,5 +450,27 @@ mod tests {
             negq!(deref!(RSP, i32::MAX as i64)),
             vec![0x48, 0xF7, 0x9C, 0x24, 0xFF, 0xFF, 0xFF, 0x7F]
         );
+    }
+
+    mod push {
+        use super::*;
+
+        check!(reg1, pushq!(reg!(RAX)), vec![0x50]);
+        check!(reg2, pushq!(reg!(R14)), vec![0x41, 0x56]);
+
+        check!(deref1, pushq!(deref!(RDX, i32::MAX as i64)), vec![0xFF, 0xB2, 0xFF, 0xFF, 0xFF, 0x7F]);
+        check!(deref2, pushq!(deref!(R11, i32::MAX as i64)), vec![0x41, 0xFF, 0xB3, 0xFF, 0xFF, 0xFF, 0x7F ]);
+
+        check!(imm, pushq!(imm!(i32::MAX as i64)), vec![ 0x68, 0xFF, 0xFF, 0xFF, 0x7F]);
+    }
+
+    mod pop {
+        use super::*;
+
+        check!(reg1, popq!(reg!(RAX)), vec![0x58 ]);
+        check!(reg2, popq!(reg!(R14)), vec![ 0x41, 0x5E]);
+
+        check!(deref1, popq!(deref!(RDX, i32::MAX as i64)), vec![0x8F, 0x82, 0xFF, 0xFF, 0xFF, 0x7F]);
+        check!(deref2, popq!(deref!(R11, i32::MAX as i64)), vec![0x41, 0x8F, 0x83, 0xFF, 0xFF, 0xFF, 0x7F]);
     }
 }
