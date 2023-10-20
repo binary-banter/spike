4c7ada994cdd85212581b4818c1a25f7a7039d6e
Vlamonster <jellemartijndijkstra@gmail.com>
2023-10-20 18:57:26 +0200

    Add Collatz test.

--- src/parser/identifier.rs
+++ src/parser/identifier.rs
@@ -1,10 +1,21 @@
 use crate::parser::re_find;
+use nom::error::ErrorKind;
 use nom::IResult;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 static IDENTIFIER_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[a-zA-Z_]\w*").unwrap());
+const RESERVED_KEYWORDS: [&str; 7] = ["t", "f", "fn", "if", "let", "Int", "Bool"];
 
 pub fn parse_identifier(input: &str) -> IResult<&str, &str> {
-    re_find(&IDENTIFIER_REGEX)(input)
+    let (rest, sym) = re_find(&IDENTIFIER_REGEX)(input)?;
+
+    if RESERVED_KEYWORDS.contains(&sym) {
+        return Err(nom::Err::Error(nom::error::Error::new(
+            input,
+            ErrorKind::Verify,
+        )));
+    }
+
+    Ok((rest, sym))
 }
--- src/parser/var.rs
+++ src/parser/var.rs
@@ -1,19 +1,7 @@
 use crate::language::lvar::Expr;
 use crate::parser::identifier::parse_identifier;
-use nom::error::ErrorKind;
 use nom::IResult;
 
-const RESERVED_KEYWORDS: [&str; 7] = ["t", "f", "fn", "if", "let", "Int", "Bool"];
-
 pub fn parse_var(input: &str) -> IResult<&str, Expr<&str>> {
-    let (rest, sym) = parse_identifier(input)?;
-
-    if RESERVED_KEYWORDS.contains(&sym) {
-        return Err(nom::Err::Error(nom::error::Error::new(
-            input,
-            ErrorKind::Verify,
-        )));
-    }
-
-    Ok((rest, Expr::Var { sym }))
+    parse_identifier(input).map(|(rest, sym)| (rest, Expr::Var { sym }))
 }
